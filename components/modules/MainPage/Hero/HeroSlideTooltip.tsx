fd845977682288078cf5c77b954036b39e8f3f69
GLOcheffkiefGANG <oskinmatem@gmail.com>
2024-05-14 01:31:50 +0600

    create-app-0.1v

---
+++ components/modules/MainPage/Hero/HeroSlideTooltip.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from '@/styles/main-page/index.module.scss'
+import { IHeroSlideTooltip } from '@/types/main-page'
+
+const HeroSlideTooltip = ({ title, image }: IHeroSlideTooltip) => (
+  <div className={`${styles.hero__slider__slide__popup} slide-popup`}>
+    <Image
+      className={styles.hero__slider__slide__popup__img}
+      src={image}
+      alt={title}
+    />
+    <p className={styles.hero__slider__slide__popup__inner}>
+      <b className={styles.hero__slider__slide__popup__title}>{title}</b>
+      <span className={styles.hero__slider__slide__popup__price}>760 ₽</span>
+    </p>
+  </div>
+)
+
+export default HeroSlideTooltip
