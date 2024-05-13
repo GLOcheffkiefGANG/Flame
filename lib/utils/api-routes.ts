5148b224c8efbac805b82a7885cd6ef195fa7948
GLOcheffkiefGANG <oskinmatem@gmail.com>
2024-05-14 02:40:00 +0600

    create-app-0.81v

--- lib/utils/api-routes.ts
+++ lib/utils/api-routes.ts
@@ -1,5 +1,5 @@
 import { Db, MongoClient } from 'mongodb'
-import { shuffle } from './common'
+import { shuffle } from '@/lib/utils/common' // Изменил импорт
 
 export const getDbAndReqBody = async (
   clientPromise: Promise<MongoClient>,
